8ebff481533597ee464721d5c7cd3aae111f763a
Tiago Carção <tiago.carcao@gmail.com>
2019-10-07 08:28:50 +0100

    Adds a shallow MoneyTransferCommand, the command that will start all the money transfer process

---
+++ src/main/java/com/litebank/webserver/application/commands/moneytransfers/MoneyTransferCommand.java
@@ -0,0 +1,6 @@
+package com.litebank.webserver.application.commands.moneytransfers;
+
+import com.litebank.webserver.application.interfaces.Command;
+
+public class MoneyTransferCommand implements Command {
+}
---
+++ src/main/java/com/litebank/webserver/application/commands/moneytransfers/MoneyTransferCommandHandler.java
@@ -0,0 +1,11 @@
+package com.litebank.webserver.application.commands.moneytransfers;
+
+import com.litebank.webserver.application.interfaces.CommandHandler;
+
+public class MoneyTransferCommandHandler implements CommandHandler<MoneyTransferCommand> {
+
+    @Override
+    public void execute(MoneyTransferCommand command) {
+
+    }
+}
