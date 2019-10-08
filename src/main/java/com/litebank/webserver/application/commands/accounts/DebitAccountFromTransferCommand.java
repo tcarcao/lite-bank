702da2bf963b2de9392765837bc0894811fa72c1
Tiago Carção <tiago.carcao@gmail.com>
2019-10-08 08:54:54 +0100

    Adds DebitAccountFromTransferCommand and tests This command is the next in chain to achieve the transfer

---
+++ src/main/java/com/litebank/webserver/application/commands/accounts/DebitAccountFromTransferCommand.java
@@ -0,0 +1,30 @@
+package com.litebank.webserver.application.commands.accounts;
+
+import com.litebank.webserver.application.interfaces.cqrs.Command;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class DebitAccountFromTransferCommand implements Command {
+    private final BigDecimal amount;
+    private final UUID accountToDebitId;
+    private final UUID moneyTransferId;
+
+    public DebitAccountFromTransferCommand(BigDecimal amount, UUID accountToDebitId, UUID moneyTransferId) {
+        this.amount = amount;
+        this.accountToDebitId = accountToDebitId;
+        this.moneyTransferId = moneyTransferId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public UUID getAccountToDebitId() {
+        return accountToDebitId;
+    }
+
+    public UUID getMoneyTransferId() {
+        return moneyTransferId;
+    }
+}
