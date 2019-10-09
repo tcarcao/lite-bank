b5a35c402d5cf62bb8abfe8c64161b0ad4ea9f7b
Tiago Carção <tiago.carcao@gmail.com>
2019-10-09 07:50:25 +0100

    Adds MoneyTransferEventsConsumer that connects all the responses to the stream of events

---
+++ src/main/java/com/litebank/webserver/presentation/api/consumers/MoneyTransferEventsConsumer.java
@@ -0,0 +1,88 @@
+package com.litebank.webserver.presentation.api.consumers;
+
+import com.litebank.webserver.application.commands.accounts.CreditAccountFromTransferCommand;
+import com.litebank.webserver.application.commands.accounts.DebitAccountFromTransferCommand;
+import com.litebank.webserver.application.commands.moneytransfers.CompleteMoneyTransferCommand;
+import com.litebank.webserver.application.commands.moneytransfers.FailMoneyTransferDueToInsufficientFundsCommand;
+import com.litebank.webserver.application.interfaces.EventBus;
+import com.litebank.webserver.domain.model.accounts.events.AccountCreditedEvent;
+import com.litebank.webserver.domain.model.accounts.events.AccountDebitFailedDueToInsufficientFundsEvent;
+import com.litebank.webserver.domain.model.accounts.events.AccountDebitedEvent;
+import com.litebank.webserver.domain.model.moneytransfers.events.MoneyTransferCreatedEvent;
+import com.litebank.webserver.presentation.api.di.CommandQueriesFactory;
+
+public class MoneyTransferEventsConsumer {
+    private final EventBus eventBus;
+    private final String moneyTransfersStreamId;
+    private final String accountsStreamId;
+    private final CommandQueriesFactory commandQueriesFactory;
+
+    public MoneyTransferEventsConsumer(EventBus eventBus, String moneyTransfersStreamId, String accountsStreamId, CommandQueriesFactory commandQueriesFactory) {
+        this.eventBus = eventBus;
+        this.moneyTransfersStreamId = moneyTransfersStreamId;
+        this.accountsStreamId = accountsStreamId;
+        this.commandQueriesFactory = commandQueriesFactory;
+    }
+
+    public void initialize() {
+        eventBus.subscribeToStream(moneyTransfersStreamId, events -> {
+            System.out.println("Consuming money transfer: " + events);
+
+            for (var event : events) {
+                if (event instanceof MoneyTransferCreatedEvent) {
+                    handleMoneyTransferCreatedEvent((MoneyTransferCreatedEvent) event);
+                }
+            }
+        });
+
+        eventBus.subscribeToStream(accountsStreamId, events -> {
+            System.out.println("Consuming accounts: " + events);
+
+            for (var event : events) {
+                if (event instanceof AccountDebitedEvent) {
+                    handleAccountDebitedEvent((AccountDebitedEvent) event);
+                }
+                else if (event instanceof AccountCreditedEvent) {
+                    handleAccountCreditedEvent((AccountCreditedEvent) event);
+                }
+                else if (event instanceof AccountDebitFailedDueToInsufficientFundsEvent) {
+                    handleAccountDebitFailedDueToInsufficientFundsEvent((AccountDebitFailedDueToInsufficientFundsEvent) event);
+                }
+            }
+        });
+    }
+
+    private void handleMoneyTransferCreatedEvent(MoneyTransferCreatedEvent event) {
+        var handler = commandQueriesFactory.getCommandHandler(DebitAccountFromTransferCommand.class);
+        var command = new DebitAccountFromTransferCommand(event.getAmount(), event.getFromAccountId(), event.getAggregateId());
+
+        handler.execute(command);
+    }
+
+    private void handleAccountDebitedEvent(AccountDebitedEvent event) {
+        if (event.isFromTransfer()) {
+            var handler = commandQueriesFactory.getCommandHandler(CreditAccountFromTransferCommand.class);
+            var command = new CreditAccountFromTransferCommand(event.getAmount(), event.getMoneyTransferId());
+
+            handler.execute(command);
+        }
+    }
+
+    private void handleAccountCreditedEvent(AccountCreditedEvent event) {
+        if (event.isFromTransfer()) {
+            var handler = commandQueriesFactory.getCommandHandler(CompleteMoneyTransferCommand.class);
+            var command = new CompleteMoneyTransferCommand(event.getMoneyTransferId());
+
+            handler.execute(command);
+        }
+    }
+
+    private void handleAccountDebitFailedDueToInsufficientFundsEvent(AccountDebitFailedDueToInsufficientFundsEvent event) {
+        if (event.isFromTransfer()) {
+            var handler = commandQueriesFactory.getCommandHandler(FailMoneyTransferDueToInsufficientFundsCommand.class);
+            var command = new FailMoneyTransferDueToInsufficientFundsCommand(event.getMoneyTransferId());
+
+            handler.execute(command);
+        }
+    }
+}
---
+++ src/test/java/com/litebank/webserver/presentation/api/consumers/MoneyTransferEventsConsumerTest.java
@@ -0,0 +1,313 @@
+package com.litebank.webserver.presentation.api.consumers;
+
+import com.litebank.webserver.application.commands.accounts.CreditAccountFromTransferCommand;
+import com.litebank.webserver.application.commands.accounts.DebitAccountFromTransferCommand;
+import com.litebank.webserver.application.commands.moneytransfers.CompleteMoneyTransferCommand;
+import com.litebank.webserver.application.commands.moneytransfers.FailMoneyTransferDueToInsufficientFundsCommand;
+import com.litebank.webserver.application.interfaces.EventBus;
+import com.litebank.webserver.application.interfaces.cqrs.CommandHandler;
+import com.litebank.webserver.domain.model.Event;
+import com.litebank.webserver.domain.model.accounts.events.AccountCreditedEvent;
+import com.litebank.webserver.domain.model.accounts.events.AccountDebitFailedDueToInsufficientFundsEvent;
+import com.litebank.webserver.domain.model.accounts.events.AccountDebitedEvent;
+import com.litebank.webserver.domain.model.moneytransfers.events.MoneyTransferCreatedEvent;
+import com.litebank.webserver.presentation.api.di.CommandQueriesFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class MoneyTransferEventsConsumerTest {
+    @Captor
+    private ArgumentCaptor<Consumer<List<Event>>> consumerArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<Class> commandHandlerArgumentCaptor;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void moneyTransferEventsConsumer_consumesMoneyTransferCreatedEvent_shouldCallDebitAccountFromTransferCommand() {
+        // Arrange
+        var moneyTransferId = UUID.randomUUID();
+        var fromAccountId = UUID.randomUUID();
+        var toAccountId = UUID.randomUUID();
+        var amount = new BigDecimal(1);
+        var currencyCode = "EUR";
+        var event = new MoneyTransferCreatedEvent(moneyTransferId, LocalDateTime.now(ZoneOffset.UTC), 1, fromAccountId, toAccountId, amount, currencyCode);
+        var moneyTransferEvents = new ArrayList<Event>();
+
+        moneyTransferEvents.add(event);
+
+        var eventBus = Mockito.mock(EventBus.class);
+        var accountsStreamId = "AA";
+        var moneyTransfersStreamId = "BB";
+        var commandQueriesFactory = Mockito.mock(CommandQueriesFactory.class);
+        var genericCommandHandler = Mockito.mock(CommandHandler.class);
+
+        when(commandQueriesFactory.getCommandHandler(any())).thenReturn(genericCommandHandler);
+
+        var consumer = new MoneyTransferEventsConsumer(eventBus, moneyTransfersStreamId, accountsStreamId, commandQueriesFactory);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // Act
+        consumer.initialize();
+
+        // Assert
+        verify(eventBus, times(2)).subscribeToStream(captor.capture(), consumerArgumentCaptor.capture());
+        var moneyTransfersStreamConsumer = consumerArgumentCaptor.getAllValues().get(0);
+        var accountsStreamConsumer = consumerArgumentCaptor.getAllValues().get(1);
+
+        accountsStreamConsumer.accept(new ArrayList<>());
+        moneyTransfersStreamConsumer.accept(moneyTransferEvents);
+
+        verify(commandQueriesFactory).getCommandHandler(commandHandlerArgumentCaptor.capture());
+
+        assertEquals(DebitAccountFromTransferCommand.class, commandHandlerArgumentCaptor.getValue());
+    }
+
+    @Test
+    public void moneyTransferEventsConsumer_consumesAccountDebitedEvent_shouldCallCreditAccountFromTransferCommand() {
+        // Arrange
+        var moneyTransferId = UUID.randomUUID();
+        var accountId = UUID.randomUUID();
+        var amountToDebit = new BigDecimal(0.5);
+        var event = new AccountDebitedEvent(accountId, LocalDateTime.now(ZoneOffset.UTC), 1, amountToDebit, moneyTransferId);
+        var accountEvents = new ArrayList<Event>();
+
+        accountEvents.add(event);
+
+        var eventBus = Mockito.mock(EventBus.class);
+        var accountsStreamId = "AA";
+        var moneyTransfersStreamId = "BB";
+        var commandQueriesFactory = Mockito.mock(CommandQueriesFactory.class);
+        var genericCommandHandler = Mockito.mock(CommandHandler.class);
+
+        when(commandQueriesFactory.getCommandHandler(any())).thenReturn(genericCommandHandler);
+
+        var consumer = new MoneyTransferEventsConsumer(eventBus, moneyTransfersStreamId, accountsStreamId, commandQueriesFactory);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // Act
+        consumer.initialize();
+
+        // Assert
+        verify(eventBus, times(2)).subscribeToStream(captor.capture(), consumerArgumentCaptor.capture());
+        var moneyTransfersStreamConsumer = consumerArgumentCaptor.getAllValues().get(0);
+        var accountsStreamConsumer = consumerArgumentCaptor.getAllValues().get(1);
+
+        accountsStreamConsumer.accept(accountEvents);
+        moneyTransfersStreamConsumer.accept(new ArrayList<>());
+
+        verify(commandQueriesFactory).getCommandHandler(commandHandlerArgumentCaptor.capture());
+
+        assertEquals(CreditAccountFromTransferCommand.class, commandHandlerArgumentCaptor.getValue());
+    }
+
+    @Test
+    public void moneyTransferEventsConsumer_consumesAccountDebitedEventNotFromTransfer_shouldNotCallCommand() {
+        // Arrange
+        var accountId = UUID.randomUUID();
+        var amountToDebit = new BigDecimal(0.5);
+        var event = new AccountDebitedEvent(accountId, LocalDateTime.now(ZoneOffset.UTC), 1, amountToDebit);
+        var accountEvents = new ArrayList<Event>();
+
+        accountEvents.add(event);
+
+        var eventBus = Mockito.mock(EventBus.class);
+        var accountsStreamId = "AA";
+        var moneyTransfersStreamId = "BB";
+        var commandQueriesFactory = Mockito.mock(CommandQueriesFactory.class);
+        var genericCommandHandler = Mockito.mock(CommandHandler.class);
+
+        when(commandQueriesFactory.getCommandHandler(any())).thenReturn(genericCommandHandler);
+
+        var consumer = new MoneyTransferEventsConsumer(eventBus, moneyTransfersStreamId, accountsStreamId, commandQueriesFactory);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // Act
+        consumer.initialize();
+
+        // Assert
+        verify(eventBus, times(2)).subscribeToStream(captor.capture(), consumerArgumentCaptor.capture());
+        var moneyTransfersStreamConsumer = consumerArgumentCaptor.getAllValues().get(0);
+        var accountsStreamConsumer = consumerArgumentCaptor.getAllValues().get(1);
+
+        accountsStreamConsumer.accept(accountEvents);
+        moneyTransfersStreamConsumer.accept(new ArrayList<>());
+
+        verify(commandQueriesFactory, never()).getCommandHandler(any());
+    }
+
+    @Test
+    public void moneyTransferEventsConsumer_consumesAccountCreditedEvent_shouldCallCompleteMoneyTransferCommand() {
+        // Arrange
+        var moneyTransferId = UUID.randomUUID();
+        var accountId = UUID.randomUUID();
+        var amountToDebit = new BigDecimal(0.5);
+        var event = new AccountCreditedEvent(accountId, LocalDateTime.now(ZoneOffset.UTC), 1, amountToDebit, moneyTransferId);
+        var accountEvents = new ArrayList<Event>();
+
+        accountEvents.add(event);
+
+        var eventBus = Mockito.mock(EventBus.class);
+        var accountsStreamId = "AA";
+        var moneyTransfersStreamId = "BB";
+        var commandQueriesFactory = Mockito.mock(CommandQueriesFactory.class);
+        var genericCommandHandler = Mockito.mock(CommandHandler.class);
+
+        when(commandQueriesFactory.getCommandHandler(any())).thenReturn(genericCommandHandler);
+
+        var consumer = new MoneyTransferEventsConsumer(eventBus, moneyTransfersStreamId, accountsStreamId, commandQueriesFactory);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // Act
+        consumer.initialize();
+
+        // Assert
+        verify(eventBus, times(2)).subscribeToStream(captor.capture(), consumerArgumentCaptor.capture());
+        var moneyTransfersStreamConsumer = consumerArgumentCaptor.getAllValues().get(0);
+        var accountsStreamConsumer = consumerArgumentCaptor.getAllValues().get(1);
+
+        accountsStreamConsumer.accept(accountEvents);
+        moneyTransfersStreamConsumer.accept(new ArrayList<>());
+
+        verify(commandQueriesFactory).getCommandHandler(commandHandlerArgumentCaptor.capture());
+
+        assertEquals(CompleteMoneyTransferCommand.class, commandHandlerArgumentCaptor.getValue());
+    }
+
+    @Test
+    public void moneyTransferEventsConsumer_consumesAccountCreditedEventNotFromTransfer_shouldNotCallCommand() {
+        // Arrange
+        var accountId = UUID.randomUUID();
+        var amountToDebit = new BigDecimal(0.5);
+        var event = new AccountCreditedEvent(accountId, LocalDateTime.now(ZoneOffset.UTC), 1, amountToDebit);
+        var accountEvents = new ArrayList<Event>();
+
+        accountEvents.add(event);
+
+        var eventBus = Mockito.mock(EventBus.class);
+        var accountsStreamId = "AA";
+        var moneyTransfersStreamId = "BB";
+        var commandQueriesFactory = Mockito.mock(CommandQueriesFactory.class);
+        var genericCommandHandler = Mockito.mock(CommandHandler.class);
+
+        when(commandQueriesFactory.getCommandHandler(any())).thenReturn(genericCommandHandler);
+
+        var consumer = new MoneyTransferEventsConsumer(eventBus, moneyTransfersStreamId, accountsStreamId, commandQueriesFactory);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // Act
+        consumer.initialize();
+
+        // Assert
+        verify(eventBus, times(2)).subscribeToStream(captor.capture(), consumerArgumentCaptor.capture());
+        var moneyTransfersStreamConsumer = consumerArgumentCaptor.getAllValues().get(0);
+        var accountsStreamConsumer = consumerArgumentCaptor.getAllValues().get(1);
+
+        accountsStreamConsumer.accept(accountEvents);
+        moneyTransfersStreamConsumer.accept(new ArrayList<>());
+
+        verify(commandQueriesFactory, never()).getCommandHandler(any());
+    }
+
+    @Test
+    public void moneyTransferEventsConsumer_consumesAccountDebitFailedDueToInsufficientFundsEvent_shouldCallFailMoneyTransferDueToInsufficientFundsCommand() {
+        // Arrange
+        var moneyTransferId = UUID.randomUUID();
+        var accountId = UUID.randomUUID();
+        var balance = new BigDecimal(10);
+        var amountToDebit = new BigDecimal(0.5);
+        var event = new AccountDebitFailedDueToInsufficientFundsEvent(accountId, LocalDateTime.now(ZoneOffset.UTC), 1, amountToDebit, balance, moneyTransferId);
+        var accountEvents = new ArrayList<Event>();
+
+        accountEvents.add(event);
+
+        var eventBus = Mockito.mock(EventBus.class);
+        var accountsStreamId = "AA";
+        var moneyTransfersStreamId = "BB";
+        var commandQueriesFactory = Mockito.mock(CommandQueriesFactory.class);
+        var genericCommandHandler = Mockito.mock(CommandHandler.class);
+
+        when(commandQueriesFactory.getCommandHandler(any())).thenReturn(genericCommandHandler);
+
+        var consumer = new MoneyTransferEventsConsumer(eventBus, moneyTransfersStreamId, accountsStreamId, commandQueriesFactory);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // Act
+        consumer.initialize();
+
+        // Assert
+        verify(eventBus, times(2)).subscribeToStream(captor.capture(), consumerArgumentCaptor.capture());
+        var moneyTransfersStreamConsumer = consumerArgumentCaptor.getAllValues().get(0);
+        var accountsStreamConsumer = consumerArgumentCaptor.getAllValues().get(1);
+
+        accountsStreamConsumer.accept(accountEvents);
+        moneyTransfersStreamConsumer.accept(new ArrayList<>());
+
+        verify(commandQueriesFactory).getCommandHandler(commandHandlerArgumentCaptor.capture());
+
+        assertEquals(FailMoneyTransferDueToInsufficientFundsCommand.class, commandHandlerArgumentCaptor.getValue());
+    }
+
+    @Test
+    public void moneyTransferEventsConsumer_consumesAccountDebitFailedDueToInsufficientFundsEventNotFromTransfer_shouldNotCallCommand() {
+        // Arrange
+        var accountId = UUID.randomUUID();
+        var balance = new BigDecimal(10);
+        var amountToDebit = new BigDecimal(0.5);
+        var event = new AccountDebitFailedDueToInsufficientFundsEvent(accountId, LocalDateTime.now(ZoneOffset.UTC), 1, amountToDebit, balance);
+        var accountEvents = new ArrayList<Event>();
+
+        accountEvents.add(event);
+
+        var eventBus = Mockito.mock(EventBus.class);
+        var accountsStreamId = "AA";
+        var moneyTransfersStreamId = "BB";
+        var commandQueriesFactory = Mockito.mock(CommandQueriesFactory.class);
+        var genericCommandHandler = Mockito.mock(CommandHandler.class);
+
+        when(commandQueriesFactory.getCommandHandler(any())).thenReturn(genericCommandHandler);
+
+        var consumer = new MoneyTransferEventsConsumer(eventBus, moneyTransfersStreamId, accountsStreamId, commandQueriesFactory);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+
+        // Act
+        consumer.initialize();
+
+        // Assert
+        verify(eventBus, times(2)).subscribeToStream(captor.capture(), consumerArgumentCaptor.capture());
+        var moneyTransfersStreamConsumer = consumerArgumentCaptor.getAllValues().get(0);
+        var accountsStreamConsumer = consumerArgumentCaptor.getAllValues().get(1);
+
+        accountsStreamConsumer.accept(accountEvents);
+        moneyTransfersStreamConsumer.accept(new ArrayList<>());
+
+        verify(commandQueriesFactory, never()).getCommandHandler(any());
+    }
+}
